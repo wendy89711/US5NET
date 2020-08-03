b3f5584c5554efc96e7bf5ced9eedeed5d7efdf1
Wendy Wu <wendy8977@outlook.com>
2020-08-03 18:25:38 +0800

    新增QA頁面

---
+++ QuerSyst/Controllers/US2000/QAEditController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace US5NET.Controllers
+{
+    public class QAEditController : Controller
+    {
+        // GET: QAEdit
+        public ActionResult QAEdit()
+        {
+            return View();
+        }
+    }
+}
